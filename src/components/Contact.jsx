6de52e366cc9dea6adf2e93080e970bd62982393
micckey <83111475+micckey@users.noreply.github.com>
2024-05-01 13:30:04 +0300

    toast

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -82,7 +82,7 @@ export default function Contact() {
             <a
               href="mikecv.pdf"
               download
-              className="mt-8 border border-blanchedalmond lg:mr-72 px-10 py-3 rounded text-antiquewhite bg-gray-800 transition-colors duration-500 hover:bg-gray-600"
+              className="mt-8 border text-center border-blanchedalmond lg:mr-72 px-10 py-3 rounded text-antiquewhite bg-gray-800 transition-colors duration-500 hover:bg-gray-600"
             >
               Download CV
             </a>
